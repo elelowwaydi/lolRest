d780c858174ee9b11ba172cf9f85b11c65d7119e
elelowwaydi <lloydaaron.barnacha@yahoo.com>
2016-06-03 02:19:59 +0800

    added 'add a record' function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 $(function() {
-  displayRecords();
+    displayRecords();
 });
 
 // cache elements
@@ -7,30 +7,53 @@ const $records = $('#records');
 const $add = $('#add');
 const $edit = $("#edit");
 const $delete = $('#delete');
-
+const $btnAdd = $('#btn-add');
+const $id = $('#id');
+const $name = $('#name');
+const $drinks = $('#drinks');
+const $addModal = $('#addModal');
 //get and display records
 function displayRecords() {
-  $.ajax({
-    type: 'GET',
-    url: 'http://rest.learncode.academy/api/learncode/friends',
-    success: function(data) {
-      var len = data.length;
+    $.ajax({
+        type: 'GET',
+        url: 'http://rest.learncode.academy/api/learncode/friends',
+        success: function(data) {
+            var len = data.length;
 
-      for(var i=0; i<len; i++) {
-        $records.append('<tr><td>' + data[i].id + '</td><td>' + data[i].name + '</td><td>' + data[i].drink + '</td></tr>');
-      }
-    }
-  });
+            for (var i = 0; i < len; i++) {
+                $records.append('<tr><td>' + data[i].id + '</td><td>' + data[i].name + '</td><td>' + data[i].drink + '</td></tr>');
+            }
+        }
+    });
 }
 
+// hover animations
 $add.hover(function() {
-  $(this).find(".glyphicon").toggleClass("scale");
+    $(this).find(".glyphicon").toggleClass("scale");
 });
 
 $edit.hover(function() {
-  $(this).find(".glyphicon").toggleClass("write");
+    $(this).find(".glyphicon").toggleClass("write");
 });
 
 $delete.hover(function() {
-  $(this).find(".glyphicon").toggleClass("rotate");
+    $(this).find(".glyphicon").toggleClass("rotate");
+});
+
+// add record
+$btnAdd.click(function() {
+    $.ajax({
+        type: 'POST',
+        url: 'http://rest.learncode.academy/api/learncode/friends',
+        data: {
+            id: $id.val(),
+            name: $name.val(),
+            drink: $drinks.val()
+        },
+        success: function(data) {
+            console.log("Friend added!", data); //the new item is returned with an ID
+        }
+    });
+
+    $addModal.modal('toggle');
 });
