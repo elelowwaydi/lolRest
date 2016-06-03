e0fd29b267b65c1642881b0d603de19786e0b71b
elelowwaydi <lloydaaron.barnacha@yahoo.com>
2016-06-04 01:28:19 +0800

    added delete record functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ $(function() {
     displayRecords();
 });
 
+
 // cache elements
 const $records = $('#records');
 const $add = $('#add');
@@ -10,8 +11,10 @@ const $delete = $('#delete');
 const $btnAdd = $('#btn-add');
 const $age = $('#age');
 const $name = $('#name');
-const $drinks = $('#drinks');
+const $drink = $('#drink');
 const $addModal = $('#addModal');
+const $editModal = $('#editModal');
+const $btnEdit = $('#btn-edit');
 
 //get and display records
 function displayRecords() {
@@ -22,7 +25,7 @@ function displayRecords() {
             var len = data.length;
 
             for (var i = 0; i < len; i++) {
-                $records.append('<tr><td>' + data[i].id + '</td><td>' + data[i].name + '</td><td>' + data[i].age + '</td><td>' + data[i].drink + '</td></tr>');
+                $records.append('<tr class="data-row"><td class="data-id">' + data[i].id + '</td><td class="data-name">' + data[i].name + '</td><td class="data-age">' + data[i].age + '</td><td class="data-drink">' + data[i].drink + '</td></tr>');
             }
         }
     });
@@ -49,13 +52,13 @@ $btnAdd.click(function() {
         data: {
             name: $name.val(),
             age: $age.val(),
-            drink: $drinks.val()
+            drink: $drink.val()
         },
         success: function(data) {
             retreiveData();
         }
     });
-
+    // close modal
     $addModal.modal('toggle');
 });
 
@@ -66,7 +69,7 @@ function retreiveData() {
         url: 'http://rest.learncode.academy/api/lloydaaron/friends',
         success: function(data) {
             var last = data.length - 1;
-            $records.append('<tr><td>' + data[last].id + '</td><td>' + data[last].name + '</td><td>' + data[last].age + '</td><td>' + data[last].drink + '</td></tr>');
+            $records.append('<tr class="data-row"><td class="data-id">' + data[last].id + '</td><td class="data-name">' + data[last].name + '</td><td class="data-age">' + data[last].age + '</td><td class="data-drink">' + data[last].drink + '</td></tr>');
         }
     });
 }
@@ -74,14 +77,71 @@ function retreiveData() {
 
 
 // select data
-$(document).on("click", "tr", function() {
+$(document).on("click", ".data-row", function() {
     $('tr').css("background-color", "#fff");
     $(this).css("background-color", "#F4F4F4");
     $edit.addClass("editable");
     $delete.addClass("deletable");
+    dataId = $(this).find(".data-id").html();
+    dataName = $(this).find(".data-name").html();
+    dataAge = $(this).find(".data-age").html();
+    dataDrink = $(this).find(".data-drink").html();
+
 });
 
+
 $("html").not("tr").click(function() {
-  $edit.removeClass("editable");
-  $delete.removeClass("deletable");
+    $edit.removeClass("editable");
+    $delete.removeClass("deletable");
+});
+
+// edit record
+$edit.click(function() {
+    $editModal.modal();
+    $editModal.find("#name").val(dataName);
+    $editModal.find("#age").val(dataAge);
+    $editModal.find("#drink").val(dataDrink);
+});
+
+
+$btnEdit.click(function() {
+    var newDataName = $editModal.find("#name").val();
+    var newDataAge = $editModal.find("#age").val();
+    var newDataDrink = $editModal.find("#drink").val();
+
+    $.ajax({
+        type: 'PUT',
+        data: {
+            name: newDataName,
+            age: newDataAge,
+            drink: newDataDrink
+        },
+        url: "http://rest.learncode.academy/api/lloydaaron/friends/" + dataId,
+        success: function(data) {
+            console.log('Record Updated Successfully!');
+            $('#records tr').detach();
+            displayRecords();
+        },
+        error: function() {
+            alert("error updating!");
+        }
+    });
+
+    // close modal
+    $editModal.modal('toggle');
+});
+
+
+// delete record
+$delete.click(function() {
+    $.ajax({
+        type: 'DELETE',
+        url: 'http://rest.learncode.academy/api/lloydaaron/friends/' + dataId,
+        success: function() {
+            //no data...just a success (200) status code
+            $records.find(".data-row:contains("+dataId+")").remove();
+            console.log('record Deleted Successfully!');
+
+        }
+    });
 });
